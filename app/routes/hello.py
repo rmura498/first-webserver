935fa37f9dc3dd4a8b866f8039a689d0983f5fa6
rmura498 <76777353+rmura498@users.noreply.github.com>
2022-01-02 22:42:01 +0100

    modifying templates

--- app/routes/hello.py
+++ app/routes/hello.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Maura'}
+    students = [
+        {'name': 'A', 'id': 1},
+        {'name': 'C', 'id': 111},
+    ]
+    return render_template('index.html', title='Home', user=user, students=students, greeting="Yo")
