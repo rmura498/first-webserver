3854baebc7352c322a4a5f32b4dddbb766c7b363
rmura498 <76777353+rmura498@users.noreply.github.com>
2022-01-03 12:24:13 +0100

    last commit

--- app/routes/hello.py
+++ app/routes/hello.py
@@ -7,8 +7,8 @@ from app import app
 def index():
     user = {'username': 'Maura'}
     students = [
-        {'name': 'A', 'id': 1},
-        {'name': 'C', 'id': 111},
+        {'name': 'Maura Puddu'},
+
     ]
     return render_template('index.html', title='Home', user=user, students=students, greeting="Ciao")
 
